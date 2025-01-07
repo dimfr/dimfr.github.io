26c92e8e976c3635fa4a2c1a669fd5c355bb427d
dimfr <franz.dmitrij@gmail.com>
2025-01-07 19:47:31 +0100

    dsdgd

--- mediazone.js
+++ mediazone.js
@@ -1802,7 +1802,7 @@
           _this2.selectedItemsHistorie.push(item);
           if (item.streamUrl != undefined && item.streamUrl != '') {
             //item.streamUrl = 'https://mediaaly.pro/tvseries/9f0209f2b0bceaeb70a815fc0b08d0c4a4bca54b/1d22572953fa6a00f953660cf7bfab23:2025010720/hls.m3u8';
-            item.streamUrl = 'https://cda-3cd-205gv.video-ik-ok-ii.space/m2/67/1/1736295989/UNRkgdeFktnQE9QuMOcRJA/2/1080/1/994756/index-f1-v1-a1.m3u8';
+            //item.streamUrl = 'https://cda-3cd-205gv.video-ik-ok-ii.space/m2/67/1/1736295989/UNRkgdeFktnQE9QuMOcRJA/2/1080/1/994756/index-f1-v1-a1.m3u8';
             var video = {
               title: item.title,
               url: item.streamUrl
